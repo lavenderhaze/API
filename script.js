773c607a028c626190bbb64000aa409c109f38da
lavenderhaze <118069971+lavenderhaze@users.noreply.github.com>
2023-06-18 15:44:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var video = document.getElementById('video');
-  var fileInput = document.getElementById('file-input');
-  var message = document.getElementById('message');
-  var playButton = document.getElementById('play-button');
-  var pauseButton = document.getElementById('pause-button');
-  var volumeUpButton = document.getElementById('volume-up-button');
-  var volumeDownButton = document.getElementById('volume-down-button');
-
-  fileInput.addEventListener('change', function(e) {
-    var file = e.target.files[0];
-    var fileURL = URL.createObjectURL(file);
-    video.src = fileURL;
-    message.style.display = 'block';
-  });
-
-  video.addEventListener('loadeddata', function() {
-    message.style.display = 'none';
-  });
-
-  playButton.addEventListener('click', function() {
-    video.play();
-  });
-
-  pauseButton.addEventListener('click', function() {
-    video.pause();
-  });
-
-  volumeUpButton.addEventListener('click', function() {
-    if (video.volume < 1) {
-      video.volume += 0.1;
+document.addEventListener("DOMContentLoaded", function() {
+    const fileInput = document.getElementById("file-input");
+    const videoPlayer = document.getElementById("video-player");
+    const playButton = document.getElementById("play-button");
+    const pauseButton = document.getElementById("pause-button");
+    const volumeUpButton = document.getElementById("volume-up-button");
+    const volumeDownButton = document.getElementById("volume-down-button");
+  
+    fileInput.addEventListener("change", function(event) {
+      const file = event.target.files[0];
+  
+      if (file.type.includes("video")) {
+        const reader = new FileReader();
+  
+        reader.onloadstart = function() {
+          alert(`Cargando el archivo: ${file.name} (${formatBytes(file.size)})`);
+        };
+  
+        reader.onload = function(event) {
+          videoPlayer.src = URL.createObjectURL(file);
+        };
+  
+        reader.readAsArrayBuffer(file);
+      } else {
+        alert("¡Error! Solo se permiten archivos de vídeo.");
+      }
+    });
+  
+    playButton.addEventListener("click", function() {
+      videoPlayer.play();
+    });
+  
+    pauseButton.addEventListener("click", function() {
+      videoPlayer.pause();
+    });
+  
+    volumeUpButton.addEventListener("click", function() {
+      videoPlayer.volume += 0.1;
+    });
+  
+    volumeDownButton.addEventListener("click", function() {
+      videoPlayer.volume -= 0.1;
+    });
+  
+    function formatBytes(bytes) {
+      const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+      if (bytes === 0) return "0 Bytes";
+      const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+      return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
     }
   });
-
-  volumeDownButton.addEventListener('click', function() {
-    if (video.volume > 0) {
-      video.volume -= 0.1;
-    }
-  });
-});
+  
